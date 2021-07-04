85c0f677806ef18c1457fe35236902775f5727f0
AlxMancia <alx.mancia@outlook.com>
2021-07-03 18:23:12 -0600

    Webserver

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,1 +1,42 @@
-//Ejemplo6
+const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();   
+
+const app = express();
+const port = process.env.PORT;
+
+//Handlebars
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/',  (req, res) => {
+  res.render('home',{
+    nombre: 'Alexis Mancia',
+    titulo: 'Curso de node'
+  });
+});
+
+app.get('/generic',  (req, res) => {
+  res.render('generic',{
+    nombre: 'Alexis Mancia',
+    titulo: 'Curso de node'
+  });
+});
+ 
+app.get('/elements',  (req, res) => {
+  res.render('elements',{
+    nombre: 'Alexis Mancia',
+    titulo: 'Curso de node'
+  });
+});
+
+app.get('*',  (req, res) => {
+  res.sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () =>{
+  console.log(`Example app listening on http://localhost:${port}`)
+});
